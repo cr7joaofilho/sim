d9689509e37243a4609a9a0ace6e349a37d1bc14
cr7joaofilho <joaovitorsilvafilho96@gmail.com>
2024-10-01 12:17:59 -0300

    gfh

---
+++ main.js
@@ -0,0 +1,13 @@
+(20) ['joao', 'marima', 'felipe', 'barbosa', 'luis', 'diogo', 'lucas', 'paulo', 'neymar', 'carlos', 'danilo', 'vinicius', 'natan', 'juia', 'julia', 'diego', 'geovana', 'gabriel', 'gustavo', 'caio']
+
+
+for (let i=0;i<nome.length;i++) {
+    console.log(i);
+    console.log(nome[i]);
+
+}
+
+let idade=23;
+
+
+
